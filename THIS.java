d1a42c66409bab61eab894df94fa8e26a9a2bb9d
Ayush Rathore <rayush2504@gmail.com>
2024-01-25 17:39:33 +0530

    first program on this keyword of java

---
+++ THIS.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class THIS {
+    String name;
+
+    public String get_name(){
+        return name;
+    }
+    public void set_Name(String str){
+        name=str;
+    }
+    public void display(){
+        System.out.println(name);
+    }
+    public static void main(String[] args){
+        THIS obj=new THIS();
+        obj.set_Name("ABC");
+        System.out.println(obj.get_name());
+        obj.display();
+    }
+}
