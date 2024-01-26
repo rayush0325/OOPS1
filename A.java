d49db5df214607d039d0a24fb8d926b6b15373aa
rayush0325 <145927717+rayush0325@users.noreply.github.com>
2024-01-26 18:15:53 +0530

    default access modifier

--- A.java
+++ A.java
@@ -1,12 +1,12 @@
 package pack1;
 
 public class A {
-    private void mA(){
+     void mA(){
         System.out.println("mA A pack1");
     }
     public static void main(String[] args){
         System.out.println("main A pack1");
-        new A1();
+        new A1();//calling default constructor of class A1
 
 
     }
@@ -16,7 +16,7 @@ class A1 extends A{
     A1(){
         A obj=new A();
         obj.mA();
-        // method mA() has private access in A
+        //now the method mA  is accessible in this class since it is declared as default
     }
 
 }
