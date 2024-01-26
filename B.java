712f6a8d0809b8da6d01bb41bbb95ff74d5a782f
rayush0325 <145927717+rayush0325@users.noreply.github.com>
2024-01-26 20:07:38 +0530

    public access modifier

--- B.java
+++ B.java
@@ -1,11 +1,12 @@
 package pack2;
 import pack1.*;
 
-public class B extends A{
+public class B {
     public static void main(String[]  args) {
-        B obj = new B();
+        A obj = new A();
         obj.mA();
     }
-    //we are accessing the method mA() of class A of package pack1 in package pack2 through inheritance.
-    // This access is possible because method mA() is declared as protected
+    //we are accessing the method mA() of class A of package pack1 in package pack2
+    //this time class B is not the subclass of A
+    // This access is possible because method mA() is declared as public
 }
