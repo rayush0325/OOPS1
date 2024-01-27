5c770073339af7e5f95636f6a38bf4a1a1ec7e0d
rayush0325 <145927717+rayush0325@users.noreply.github.com>
2024-01-27 17:30:24 +0530

    Static method explanation

--- Static.java
+++ Static.java
@@ -1,35 +1,29 @@
 import javax.swing.plaf.synth.SynthLookAndFeel;
 
 public class Static {
-
-    static int x=0;//static variable created at class level
-
-    public void m(){
-        System.out.println(x);//static variable can be accessed in non-static members of class
+    static int x=0;
+    int y=1;
+    public static void m(){
+        System.out.println("Static method ");
+    }
+    public void m1(){
+        System.out.println("non-static method ");
     }
 
     public static void main(String[] args){
-        System.out.println("main method");
-        x=1;
-        Static obj=new Static();
-        obj.x=2;
-        System.out.println("Static.x :"+Static.x);//static variable are accessible with class name
-        System.out.println("x :"+x);//accessing without object
-        System.out.println("obj.x :"+obj.x);//accessing with object
-        Static obj1 =new Static();
-        System.out.println("obj1.x :"+obj1.x);//change in value of static variable x is reflected in all objects
-        Static obj2 =new Static();
-        obj2.m();
-        System.out.println("y :"+Static1.y);//accessing static variable y of class Static1 with its class name
-        Static1 obj3=new Static1();
-        System.out.println("obj3 :"+obj3.y);
+        System.out.println("accessing static method");
+        m();//static methods are accessible directly within static methods
+        Static.m();//static methods are accessible with class name
+        Static obj=new Static();//
+        obj.m();//static methods are accessible with object reference
+        System.out.println(x);//static method (here main()) can only access static variable directly
+        System.out.println(obj.y);//non-static methods and variable can be accessed in non-static methods only through object reference
+        obj.m1();
+
 
     }
 }
-class Static1{
-    static int y=0;//created at class level
 
-}
 
 
 
